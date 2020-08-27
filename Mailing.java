b73dca2777ca5950943401ca6418ef3f1c90d12f
shivm00 <shivambpatel@gmail.com>
2020-08-27 15:33:47 -0400

    Add files via upload
    spigot version: 1.16.2

---
+++ Mailing.java
@@ -0,0 +1,45 @@
+package Test_Plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Mailing extends  JavaPlugin {
+
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+	
+	public boolean receiveEnvelope(){
+		return false; //placeholder
+	}
+	
+	public boolean putItemsIntoEnvelope(){
+		return false; //placeholder
+	}
+	
+	public boolean provideNameToSend(){
+		return false; //placeholder
+	}
+	
+	public boolean sendEnvelope(){
+		return false; //placeholder
+	}
+	
+	public void sendNotificationToReceipient(){
+		
+	}
+	
+	public boolean openInventoryofEnvelope(){
+		return false; //placeholder
+	}
+	
+	
+	
+	
+	
+}
