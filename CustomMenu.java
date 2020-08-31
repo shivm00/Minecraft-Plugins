283f343b5330dbfdd566f3f6a8c99565b6c763f7
shivm00 <shivambpatel@gmail.com>
2020-08-30 23:48:17 -0400

    Add files via upload

---
+++ CustomMenu.java
@@ -0,0 +1,50 @@
+package Test_Plugin;
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class CustomMenu implements Listener{
+
+	public static void openGUI (Player player){
+		Inventory inventory= Bukkit.createInventory(null, 1, "Command");
+		ItemStack envelope = new ItemStack(Material.WHITE_WOOL);
+		ItemMeta envelopeMeta = envelope.getItemMeta();
+		envelope.setItemMeta(envelopeMeta);
+		ArrayList sLore = new ArrayList<>();
+		String lore = "Click here to open envelope";
+		sLore.add(lore);
+		envelopeMeta.setLore(sLore);
+		
+		inventory.setItem(1,envelope);
+		
+	}
+	
+	@EventHandler
+	public void inventoryClick(InventoryClickEvent event){
+		Inventory envelopeContents = Bukkit.createInventory(null, 20, ChatColor.RED + "Envelope");
+		
+		if(event.getWhoClicked() instanceof Player){
+			Player p = (Player) event.getWhoClicked();
+			ItemStack item = event.getCurrentItem();
+			
+			if(item.hasItemMeta()){ //if it has item meta
+				if(event.getRawSlot()==1){
+					p.openInventory(envelopeContents);
+				}
+			}
+		}
+		
+		
+	}
+}
